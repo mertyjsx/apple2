cb27bd0351ea5e15bc08ca041636fd3b2edd5784
orkunmertgs98 <orkunmertgs41@gmail.com>
2020-08-09 17:09:12 +0300

    first

---
+++ components/ImageAnimation.js
@@ -0,0 +1,40 @@
+import Head from 'next/head'
+import {useEffect,useState} from "react"
+import styles from '../styles/Home.module.css'
+import { urlObjectKeys } from 'next/dist/next-server/lib/utils';
+import Lg from "./lg"
+import Xs from "./xs"
+export default function Home() {
+
+  const [scrollPosition, setSrollPosition] = useState(0);
+  const [fixed, set_fixed] = useState(false);
+  const [width, set_width] = useState(0);
+  const [height, set_height] = useState(0);
+  const [orginal_Width,set_o]=useState()
+const handleScroll = () => {
+
+set_width(window.innerWidth)
+
+};
+
+useEffect(() => {
+    window.addEventListener("resize", handleScroll, { passive: true });
+  
+    return () => {
+        window.removeEventListener('resize', handleScroll);
+    };
+}, []);
+
+useEffect(() => {
+ set_width(window.innerWidth)
+}, []);
+
+
+
+  if(width<600){
+return(<Xs></Xs>)
+  }
+  else{
+return(<Lg></Lg>)
+  }
+  }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-
+import ImageAnimation from "../components/ImageAnimation"
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -9,55 +9,29 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+      <main >
+      <div className="main-title justify-start">
+        <h1 className="grey-title">
+        Retina Ekran
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <h1 className={styles.title}>
+        Büyük ve parlak.
+        </h1>
+        <h1 className={styles.title}>
+        Fikirleriniz gibi.
+        </h1>
         </div>
-      </main>
+        <ImageAnimation ></ImageAnimation>
+      
 
+</main>
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+       By orkunmertgs41@gmail.com
         </a>
       </footer>
     </div>
